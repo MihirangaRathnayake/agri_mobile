cffa309bdb8dc5b53dc09e96fb64558209d64e8b
Mihiranga Rathnayake <mihirangarathnayake2006@gmail.com>
2026-02-15 21:46:00 +0530

    navigation added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 ﻿import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'screens/login_screen.dart';
-import 'screens/main_navigation.dart';
 
 void main() {
   runApp(const AgriBot());
